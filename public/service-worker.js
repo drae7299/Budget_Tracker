66d884b43961eb30d7f3eb1bcff47ab724497897
Andre Rodriguez <drae7299@gmail.com>
2021-06-11 01:23:01 -0400

    formatted document

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,58 +6,58 @@ const FILES_TO_CACHE = [
   "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
-]
+  "/icons/icon-512x512.png",
+];
 
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime-cache';
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime-cache";
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-      caches
-          .open(PRECACHE)
-          .then(cache => cache.addAll(FILES_TO_CACHE))
-          .then(self.skipWaiting())
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
   );
 });
 
-self.addEventListener('active', event => {
+self.addEventListener("active", (event) => {
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
-      caches
-          .keys()
-          .then(cachesName => {
-              return cachesName.filter(cachesName => !currentCaches.includes(cachesName));
+    caches
+      .keys()
+      .then((cachesName) => {
+        return cachesName.filter(
+          (cachesName) => !currentCaches.includes(cachesName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
           })
-          .then(cachesToDelete => {
-              return Promise.all(
-                  cachesToDelete.map(cacheToDelete => {
-                      return caches.delete(cacheToDelete);
-                  })
-              );
-          })
-          .then(() => self.ClientRectList.claim())
+        );
+      })
+      .then(() => self.ClientRectList.claim())
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   if (event.request.url.startsWith(self.location.origin)) {
-      event.respondWith(
-          caches.open(RUNTIME).then(cache => {
-              
-                  return fetch(event.request).then(response => {
-                      if (response.status = 200) {
-                          return cache.put(event.request.url, response.clone()).then(() => {
-                              return response;
-                          });
-                      }
-                      
-                  })
-                  .catch((err) => {
-                      return cache.match(event.request)
-                  })
-              
+    event.respondWith(
+      caches.open(RUNTIME).then((cache) => {
+        return fetch(event.request)
+          .then((response) => {
+            if ((response.status = 200)) {
+              return cache.put(event.request.url, response.clone()).then(() => {
+                return response;
+              });
+            }
           })
-      );
-  }
+          .catch((err) => {
+            return cache.match(event.request);
+          });
+      })
+    );
+  };
 });
