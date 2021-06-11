fef96fa32c176625d22858387e8f6f9751ccd382
Andre Rodriguez <drae7299@gmail.com>
2021-06-11 01:34:56 -0400

    fixed service worker to shaw datat on heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,87 +1,74 @@
-console.log("Hi from your service-worker.js file!");
+console.log("Greetings from your service-worker.js file!");
 
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/db.js",
-    "/index.js",
-    "/manifest.webmanifest",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-    // "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.csshttps://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" // *NOTE: causes error in console - "cannot execute addAll (FILES TO CACHE)"
+  "/",
+  "/index.html",
+  "/db.js",
+  "/index.js",
+  "/manifest.webmanifest",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
 ];
-
-//credit: 18-PWA 13-Caching + Fetching Files
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
-// install
 self.addEventListener("install", function (evt) {
-    // pre cache transaction data
-    evt.waitUntil(
-        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
-    );
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
 
-    // pre cache all static assets
-    evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-    );
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
 
-    // tell the browser to activate this service worker immediately once it
-    // has finished installing
-    self.skipWaiting();
+  self.skipWaiting();
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener("activate", function (evt) {
-    evt.waitUntil(
-        caches.keys().then(keyList => {
-            return Promise.all(
-                keyList.map(key => {
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log("Removing old cache data", key);
-                        return caches.delete(key);
-                    }
-                })
-            );
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
 
-    self.clients.claim();
+  self.clients.claim();
 });
 
-// fetch
 self.addEventListener("fetch", function (evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/transaction")) {
-        evt.respondWith(
-            caches.open(DATA_CACHE_NAME).then(cache => {
-                return fetch(evt.request)
-                    .then(response => {
-                        // If the response was good, clone it and store it in the cache.
-                        if (response.status === 200) {
-                            cache.put(evt.request.url, response.clone());
-                        }
-
-                        return response;
-                    })
-                    .catch(err => {
-                        // Network request failed, try to get it from the cache.
-                        return cache.match(evt.request);
-                    });
-            }).catch(err => console.log(err))
-        );
-
-        return;
-    }
-
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
+  if (evt.request.url.includes("/api/transaction")) {
     evt.respondWith(
-        caches.match(evt.request).then(function (response) {
-            return response || fetch(evt.request);
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
         })
+        .catch((err) => console.log(err))
     );
+
+    return;
+  }
+  evt.respondWith(
+    caches.match(evt.request).then(function (response) {
+      return response || fetch(evt.request);
+    })
+  );
 });
